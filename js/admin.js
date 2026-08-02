c552347a58bc12fecfdad7d141f36776c128f083
mid8568 <56178383@qq.com>
2026-08-02 20:47:50 +0800

    Update admin.js

--- js/admin.js
+++ js/admin.js
@@ -4,55 +4,250 @@ console.log(
 
 
 
-function openModule(module){
+
+// Supabase
 
 
-let frame =
-document.getElementById(
-"module-frame"
+const SUPABASE_URL =
+"https://ukxxmxnubxjezkwbbxdr.supabase.co";
+
+
+const SUPABASE_KEY =
+"sb_publishable_2IFHfms3ombozpvZCvaeEg_2VZ2z5hJ";
+
+
+
+const supabaseClient =
+supabase.createClient(
+SUPABASE_URL,
+SUPABASE_KEY
 );
 
 
 
-switch(module){
 
 
-case "products":
-
-frame.src =
-"admin-products.html";
-
-break;
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
 
 
+loadDashboard();
 
-case "decoration":
 
-frame.src =
-"admin-decoration.html";
-
-break;
+});
 
 
 
-case "images":
-
-frame.src =
-"admin-images.html";
-
-break;
 
 
 
-case "ebay":
 
-frame.src =
-"admin-ebay.html";
+async function loadDashboard(){
 
-break;
+
+
+// 商品数量
+
+
+let {
+
+count:total
+
+}=await supabaseClient
+
+.from("products")
+
+.select(
+"*",
+{
+count:"exact",
+head:true
+}
+);
+
+
+
+document.getElementById(
+"total-products"
+).innerText =
+total || 0;
+
+
+
+
+
+
+
+
+// 上架
+
+
+let {
+
+count:online
+
+}=await supabaseClient
+
+.from("products")
+
+.select(
+"*",
+{
+count:"exact",
+head:true
+}
+)
+
+.eq(
+"stock_status",
+"上架"
+);
+
+
+
+document.getElementById(
+"online-products"
+).innerText =
+online || 0;
+
+
+
+
+
+
+
+
+// 下架
+
+
+let {
+
+count:offline
+
+}=await supabaseClient
+
+.from("products")
+
+.select(
+"*",
+{
+count:"exact",
+head:true
+}
+)
+
+.eq(
+"stock_status",
+"下架"
+);
+
+
+
+document.getElementById(
+"offline-products"
+).innerText =
+offline || 0;
+
+
+
+
+
+
+
+
+// 图片数量
+
+
+let {
+
+data
+
+}=await supabaseClient
+
+.from("products")
+
+.select(
+"image,image2,image3,image4,detail_images"
+);
+
+
+
+
+let num=0;
+
+
+
+data.forEach(
+p=>{
+
+
+if(p.image)
+num++;
+
+
+if(p.image2)
+num++;
+
+
+if(p.image3)
+num++;
+
+
+if(p.image4)
+num++;
+
+
+
+if(
+Array.isArray(
+p.detail_images
+)
+){
+
+num+=
+p.detail_images.length;
+
+}
+
+
+});
+
+
+
+
+document.getElementById(
+"total-images"
+).innerText =
+num;
+
 
 
 }
 
 
+
+
+
+
+
+//退出
+
+function logout(){
+
+
+localStorage.removeItem(
+"admin_login"
+);
+
+
+location.href=
+"admin-login.html";
+
+
 }
+
+
+
+window.logout =
+logout;
